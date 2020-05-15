7a57258684a0a8f41eea2501ebe52f887b13ab07
Mario-Galindo <mgalindo9410@gmail.com>
2020-05-15 12:02:19 -0600

    building a modal

--- App.js
+++ App.js
@@ -1,28 +1,45 @@
 import React from 'react';
-import { View, Alert, Button } from 'react-native';
+import { View, Modal, Button, Alert, StyleSheet, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cyan:{
+    backgroundColor:'cyan'
+  },
+  gray:{
+    backgroundColor:'gray'
+  },
+  margin:{
+    margin:55
+  }
+})
 
 export default class App extends React.Component {
 
+  state = {
+    visible: false
+  }
+
   handlePress = () => {
-    Alert.alert(
-      "Soy el titulo!",
-      "Text que se incluye en la alerta",
-      [
-        { text: "Boton 1", onPress: () => Alert.alert("Botton 1 Pressed") },
-        { text: "Boton 2", onPress: () => Alert.alert("Botton 2 Pressed") }
-      ]
-    )
+    this.setState({
+      visible: !this.state.visible
+    })
   }
 
   render() {
     return (
-      <View style={{
-        justifyContent:"center",
-        alignItems:"center",
-        display:"flex",
-        flex:1
-      }}>
-        <Button title="Pinchame" onPress={this.handlePress}></Button>
+      <View style={[styles.container,styles.cyan]}>
+        <Modal animationType="slide" transparent={true} visible={this.state.visible}>
+          <View style={[styles.container,styles.gray,styles.margin]}>
+            <Button title="Cerrar Modal" onPress={this.handlePress} />
+          </View>
+        </Modal>
+        <Button title="Open Modal" onPress={this.handlePress} />
       </View>
     )
   }
