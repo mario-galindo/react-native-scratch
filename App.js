aed183dea491d29467069c5a79a59eb77c2b0e13
Mario-Galindo <mgalindo9410@gmail.com>
2020-05-14 22:12:28 -0600

    ScrollView Component

--- App.js
+++ App.js
@@ -1,66 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput,TouchableOpacity } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 
 
 export default class App extends React.Component {
 
-  state = {};
-  handleChange = text => {
-    this.setState({ text });
-  }
-
-  handlePress = () => {
-    alert(`Tu Nombre es: ${this.state.text}`)
-  }
-
-
   render() {
-
-    const { text } = this.state
-
     return (
-      <View style={styles.container}>
-        <TextInput
-          placeholder="Ingrese su nombre"
-          onChangeText={this.handleChange}
-        />
-        <TouchableOpacity onPress={this.handlePress} style={styles.button}> 
-          <Text>Aceptar</Text>
-          </TouchableOpacity>
-      </View>
+      <ScrollView>
+        <Text style={{fontSize:80}}>Mario</Text>
+        <Text style={{fontSize:80}}>Mario</Text>
+        <Text style={{fontSize:80}}>Mario</Text>
+        <Text style={{fontSize:80}}>Mario</Text>
+        <Text style={{fontSize:80}}>Mario</Text>
+        <Text style={{fontSize:80}}>Mario</Text>
+        <Text style={{fontSize:80}}>Mario</Text>
+        <Text style={{fontSize:80}}>Mario</Text>
+        <Text style={{fontSize:80}}>Mario</Text>
+        <Text style={{fontSize:80}}>Mario</Text>
+      </ScrollView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  text: {
-    height: 100,
-    width: 100,
-    color: 'red',
-    backgroundColor: 'brown'
-  },
-  text2: {
-    height: 100,
-    width: 100,
-    color: 'red',
-    backgroundColor: 'yellow'
-  },
-  text3: {
-    height: 100,
-    width: 100,
-    color: 'red',
-    backgroundColor: 'orange'
-  },
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-evenly'
-  },
-  button:{
-    backgroundColor:'cyan',
-    height:50,
-    padding:15
-  }
-});
