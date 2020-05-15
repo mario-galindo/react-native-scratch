e64c6057e0cfd3896cf7233b6ffb1f1587d934b4
Mario-Galindo <mgalindo9410@gmail.com>
2020-05-15 11:12:17 -0600

    Alert with bottons

--- App.js
+++ App.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import { View, ActivityIndicator } from 'react-native';
+import { View, Alert, Button } from 'react-native';
 
 export default class App extends React.Component {
 
+  handlePress = () => {
+    Alert.alert(
+      "Soy el titulo!",
+      "Text que se incluye en la alerta",
+      [
+        { text: "Boton 1", onPress: () => Alert.alert("Botton 1 Pressed") },
+        { text: "Boton 2", onPress: () => Alert.alert("Botton 2 Pressed") }
+      ]
+    )
+  }
+
   render() {
     return (
-      <View>
-        <ActivityIndicator
-          size="large"
-          color="#0000ff"
-        />
-        <ActivityIndicator
-          size="small"
-          color="#0000ff"
-        />
-        <ActivityIndicator
-          size="large"
-          color="#00ffff"
-        />
-        <ActivityIndicator
-          size="large"
-          color="#ff00ff"
-        />
+      <View style={{
+        justifyContent:"center",
+        alignItems:"center",
+        display:"flex",
+        flex:1
+      }}>
+        <Button title="Pinchame" onPress={this.handlePress}></Button>
       </View>
     )
   }
