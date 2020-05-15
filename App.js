51ea11cf6fde65844f026b44770b594750e2e865
Mario-Galindo <mgalindo9410@gmail.com>
2020-05-15 10:49:53 -0600

    ActivityIndicator

--- App.js
+++ App.js
@@ -1,31 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View, Picker } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-})
+import { View, ActivityIndicator } from 'react-native';
 
 export default class App extends React.Component {
-
-  state = {}
 
   render() {
     return (
       <View>
-        <Picker
-          selectedValue={this.state.value || 'id_1'}
-          style={{ height: 70, width: 150 }}
-          onValueChange={(value, index) => {
-            this.setState({ value })
-          }}>
-          <Picker.Item label="Chanchito Feliz" value="id_1" />
-          <Picker.Item label="Fluffykins" value="id_2" />
-        </Picker>
+        <ActivityIndicator
+          size="large"
+          color="#0000ff"
+        />
+        <ActivityIndicator
+          size="small"
+          color="#0000ff"
+        />
+        <ActivityIndicator
+          size="large"
+          color="#00ffff"
+        />
+        <ActivityIndicator
+          size="large"
+          color="#ff00ff"
+        />
       </View>
     )
   }
