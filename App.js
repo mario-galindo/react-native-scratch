4c34269020133525fe5d189bc7fd6aa5986d2b88
Mario-Galindo <mgalindo9410@gmail.com>
2020-05-14 22:31:05 -0600

    Using FlatList to render arrays

--- App.js
+++ App.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { Text, View, ScrollView } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
 
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <ScrollView>
-        <Text style={{fontSize:80}}>Mario</Text>
-        <Text style={{fontSize:80}}>Mario</Text>
-        <Text style={{fontSize:80}}>Mario</Text>
-        <Text style={{fontSize:80}}>Mario</Text>
-        <Text style={{fontSize:80}}>Mario</Text>
-        <Text style={{fontSize:80}}>Mario</Text>
-        <Text style={{fontSize:80}}>Mario</Text>
-        <Text style={{fontSize:80}}>Mario</Text>
-        <Text style={{fontSize:80}}>Mario</Text>
-        <Text style={{fontSize:80}}>Mario</Text>
-      </ScrollView>
+      <View>
+        <FlatList
+          data={[
+            { name: 'Chanchito Feliz', key: "1" },
+            { name: 'Fluffy', key: "2" },
+            { name: 'Mr Algore', key: "3" },
+            { name: 'Buttersnip', key: "4" },
+            { name: 'Feliper', key: "5" },
+            { name: 'Eduardo', key: "6" },
+            { name: 'Eduardo', key: "7" },
+            { name: 'Eduardo', key: "8" },
+            { name: 'Eduardo', key: "9" },
+            { name: 'Eduardo', key: "10" },
+            { name: 'Eduardo', key: "11" },
+            { name: 'Eduardo', key: "12" }
+          ]}
+          renderItem={({ item }) => <Text style={{ fontSize: 80 }}>{item.name}</Text>}
+        ></FlatList>
+      </View>
     );
   }
 }
