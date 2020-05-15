4e9f6181b3515f8d453a476516e79a2b840e4b90
Mario-Galindo <mgalindo9410@gmail.com>
2020-05-14 22:47:18 -0600

    adding "SectionLists" componentes to group data

--- App.js
+++ App.js
@@ -1,29 +1,39 @@
 import React from 'react';
-import { Text, View, FlatList } from 'react-native';
-
+import { Text, View, SectionList } from 'react-native';
 
 export default class App extends React.Component {
 
   render() {
     return (
       <View>
-        <FlatList
-          data={[
-            { name: 'Chanchito Feliz', key: "1" },
-            { name: 'Fluffy', key: "2" },
-            { name: 'Mr Algore', key: "3" },
-            { name: 'Buttersnip', key: "4" },
-            { name: 'Feliper', key: "5" },
-            { name: 'Eduardo', key: "6" },
-            { name: 'Eduardo', key: "7" },
-            { name: 'Eduardo', key: "8" },
-            { name: 'Eduardo', key: "9" },
-            { name: 'Eduardo', key: "10" },
-            { name: 'Eduardo', key: "11" },
-            { name: 'Eduardo', key: "12" }
+        <SectionList
+          sections={[
+            {
+              title: 'Nombres', 
+              data: [
+                { name: 'Chanchito Feliz', key: "1" },
+                { name: 'Fluffy', key: "2" },
+                { name: 'Mr Algore', key: "3" },
+                { name: 'Buttersnip', key: "4" },
+                { name: 'Feliper', key: "5" }
+              ]
+            },
+            {
+              title: 'Eduardos', 
+              data: [
+                { name: 'Eduardo', key: "6" },
+                { name: 'Eduardo', key: "7" },
+                { name: 'Eduardo', key: "8" },
+                { name: 'Eduardo', key: "9" },
+                { name: 'Eduardo', key: "10" },
+                { name: 'Eduardo', key: "11" },
+                { name: 'Eduardo', key: "12" }
+              ]
+            }
           ]}
-          renderItem={({ item }) => <Text style={{ fontSize: 80 }}>{item.name}</Text>}
-        ></FlatList>
+        renderItem={({item}) => <Text style={{fontSize:40}}>{item.name}</Text>}
+        renderSectionHeader={({section}) => <Text style={{fontSize:40, backgroundColor:'gray'}}>{section.title}</Text>}
+        />
       </View>
     );
   }
