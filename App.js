30b6241abc7df51451ed0c8d622973fb89a58115
Mario-Galindo <mgalindo9410@gmail.com>
2020-05-16 21:11:43 -0600

    adding useEffects

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
 
   const [cont, setCont] = useState(0)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000);
+  },[])
 
   return (
     <View style={styles.container}>
-      <Text onPress={() => setCont(cont + 1)} style={styles.text}>{cont}</Text>
+      <Text onPress={() => setCont(cont + 1)} style={styles.text}>{loading ? 'Cargando...' : cont}</Text>
     </View>
   );
 }
